81e4f46cf003e538b4f83d3fa97ef458ede00d66
Guillermo <guille@example.com>
2024-01-23 09:55:54 +0100

    init

--- ejercicio2/src/app/app.routes.ts
+++ ejercicio2/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
+import { LenguajesComponent } from './components/lenguajes/lenguajes.component';
 
 export const routes: Routes = [
 
     {path: "home", component: HomeComponent},
     {path: "about", component: AboutComponent},
+    {path: "lenguajes", component: LenguajesComponent},
     {path: "**", pathMatch: "full", redirectTo: "home"},
 ];
